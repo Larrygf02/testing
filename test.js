7faca00e7fe1a3e0df8f8bad877096979759dc5f
larrygf02 <raulgf_02@hotmail.com>
2019-03-24 13:00:58 -0500

    Testing multiples llamadas

--- test.js
+++ test.js
@@ -1,7 +1,17 @@
 describe("pow", function() {
 
     it("raises to n-th power", function() {
-      assert.equal(pow(2, 3), 8);
+      assert.equal(pow(2, 4), 16);
     });
+    
+    it("comprobando suma", function() {
+        assert.equal(sum(2,4), 6);
+    })
+
+    //detectando multiples llamadas
+    it('double done', function(done) {
+        setImmediate(done);
+        setImmediate(done);
+    })
   
   });
